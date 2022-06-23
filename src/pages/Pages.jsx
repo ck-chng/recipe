254204706197f809d6c38bcfcae211141739876f
ck-chng <chukaichng@gmail.com>
2022-06-23 23:38:06 +0100

    first commit

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,13 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+
+function Pages() {
+    return (
+        <div>
+            <Veggie />
+            <Popular />
+        </div>
+    )
+}
+
+export default Pages
